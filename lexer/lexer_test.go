95399f53573e5e0e1032b8ce2ea2a1f0e4a2ddf0
John Durham <jdurham2843@gmail.com>
2020-02-23 18:08:32 -0600

    skipWhitespace and tokenize monkey language test code

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,28 +7,29 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `
-		let five = 5;
+	input := `let five = 5;
 		let ten = 10;
 
 		let add = fn(x, y) {
 			x + y;
 		};
 
-		let results = add(five, ten);
+		let result = add(five, ten);
 	`
 
 	tests := []struct {
 		expectType      token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "add"},
